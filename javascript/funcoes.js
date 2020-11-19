679beee8893e52d6db1493c718473232e00bc4a3
Raphael Sanches <73669213+raphael-sanches@users.noreply.github.com>
2020-11-19 19:11:08 -0300

    19/11/2020

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -2,33 +2,37 @@
 function entrada(a) {
     let icones = document.getElementById('icone-trans')
     if (a ==1) {
-    icones.innerHTML = '<img id="icone" src="_imagens/_icones/home.png" ></img>'} 
+    icones.innerHTML = '<img id="icone" src="imagens/icones/home.png" ></img>'
+    } 
     if (a ==2) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/specs.png" ></img>'}
+        icones.innerHTML = '<img id="icone" src="imagens/icones/specs.png" ></img>'
+        }
     if (a ==3) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/pictures.png" ></img>'}
+        icones.innerHTML = '<img id="icone" src="imagens/icones/pictures.png" ></img>'
+        }
     if (a ==4) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/video.png" ></img>'}
+        icones.innerHTML = '<img id="icone" src="imagens/icones/video.png" ></img>'
+        }
     if (a==5) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/contact.png" ></img>'} 
+        icones.innerHTML = '<img id="icone" src="imagens/icones/contact.png" ></img>'} 
     }
 
 function saida(b) {
     let icones = document.getElementById('icone-trans')
     if (b==1) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/glass-oculos-preto-grd.png">'
+        icones.innerHTML = '<img id="icone" src="imagens/icones/glass-oculos-preto-grd.png">'
     }
     if (b==2) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/specs.png">'
+        icones.innerHTML = '<img id="icone" src="imagens/icones/specs.png">'
     }
     if (b==3) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/pictures.png">'
+        icones.innerHTML = '<img id="icone" src="imagens/icones/pictures.png">'
     }
     if (b==4) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/video.png">'
+        icones.innerHTML = '<img id="icone" src="imagens/icones/video.png">'
     }
     if (b==5) {
-        icones.innerHTML = '<img id="icone" src="_imagens/_icones/contact.png">'
+        icones.innerHTML = '<img id="icone" src="imagens/icones/contact.png">'
     }
 }
 
@@ -50,103 +54,103 @@ if (b == 4) {
 
 function saidainterativo() {
     let specstxt = document.getElementById('conteudo-interativo')
-    specstxt.innerHTML =  '<article id="top"> <header> <h2>Passe o Mouse e Clique sobre as áreas destacadas em vermelho<img src="_imagens/mao.png" alt="" width="300px" ></h2> </header></article>' 
+    specstxt.innerHTML =  '<article id="top"> <header> <h2>Passe o Mouse e Clique sobre as áreas destacadas em vermelho<img src="imagens/mao.png" alt="" width="300px" ></h2> </header></article>' 
 }
 
 function image(a) {
     let specstxt = document.getElementById('conteudo-interativo')
     if (a == 1) {
-    specstxt.innerHTML =  '<img src="_imagens/det-tela.png" alt="" class="img-i" >'
+    specstxt.innerHTML =  '<img src="imagens/det-tela.png" alt="" class="img-i" >'
          }
     if (a == 2) {
-        specstxt.innerHTML =  '<img src="_imagens/det-camera.png" alt="" class="img-i" >'
+        specstxt.innerHTML =  '<img src="imagens/det-camera.png" alt="" class="img-i" >'
         }
         if (a == 3) {
-        specstxt.innerHTML =  '<img src="_imagens/det-touch.png" alt="" class="img-i" >'
+        specstxt.innerHTML =  '<img src="imagens/det-touch.png" alt="" class="img-i" >'
         }    
     if (a == 4) {
-        specstxt.innerHTML =  '<img src="_imagens/det-bateria.png" alt="" class="img-i" >'    
+        specstxt.innerHTML =  '<img src="imagens/det-bateria.png" alt="" class="img-i" >'    
         }
 }
 
 function zoomin (b) {
     if (b == 1) {
     let photodata = document.getElementById('foto01')
-    photodata.innerHTML = '<img src="_imagens/galeria-01.jpg" width="100px" onmouseover="zoomin(1)" onmouseout="zoomout(1)" onclick="zoom(1)">'
+    photodata.innerHTML = '<img src="imagens/galeria-01.jpg" width="100px" onmouseover="zoomin(1)" onmouseout="zoomout(1)" onclick="zoom(1)">'
     }
     if (b == 2) {
         let photodata = document.getElementById('foto02')
-        photodata.innerHTML = '<img src="_imagens/galeria-02.jpg" width="100px" onmouseover="zoomin(2)" onmouseout="zoomout(2)" onclick="zoom(2)">'
+        photodata.innerHTML = '<img src="imagens/galeria-02.jpg" width="100px" onmouseover="zoomin(2)" onmouseout="zoomout(2)" onclick="zoom(2)">'
     }
     if (b == 3) {
     let photodata = document.getElementById('foto03')
-    photodata.innerHTML = '<img src="_imagens/galeria-03.jpg"  width="100px" onmouseover="zoomin(3)" onmouseout="zoomout(3)" onclick="zoom(3)">'
+    photodata.innerHTML = '<img src="imagens/galeria-03.jpg"  width="100px" onmouseover="zoomin(3)" onmouseout="zoomout(3)" onclick="zoom(3)">'
     }
     if (b == 4) {
         let photodata = document.getElementById('foto04')
-        photodata.innerHTML = '<img src="_imagens/galeria-04.jpg"  width="100px" onmouseover="zoomin(4)" onmouseout="zoomout(4)" onclick="zoom(4)">'
+        photodata.innerHTML = '<img src="imagens/galeria-04.jpg"  width="100px" onmouseover="zoomin(4)" onmouseout="zoomout(4)" onclick="zoom(4)">'
      }
     if (b == 5) {
     let photodata = document.getElementById('foto05')
-    photodata.innerHTML = '<img src="_imagens/galeria-05.jpg"  width="100px" onmouseover="zoomin(5)" onmouseout="zoomout(5)" onclick="zoom(5)">'
+    photodata.innerHTML = '<img src="imagens/galeria-05.jpg"  width="100px" onmouseover="zoomin(5)" onmouseout="zoomout(5)" onclick="zoom(5)">'
     }
     if (b == 6) {
         let photodata = document.getElementById('foto06')
-        photodata.innerHTML = '<img src="_imagens/galeria-06.jpg" width="100px" onmouseover="zoomin(6)" onmouseout="zoomout(6)" onclick="zoom(6)">'
+        photodata.innerHTML = '<img src="imagens/galeria-06.jpg" width="100px" onmouseover="zoomin(6)" onmouseout="zoomout(6)" onclick="zoom(6)">'
     }
 }
 
 function zoomout (c) {
     if (c == 1) {
     let photodata = document.getElementById('foto01')
-    photodata.innerHTML = '<img src="_imagens/galeria-01-b.jpg" width="100px" onmouseover="zoomin(1)" onmouseout="zoomout(1)" onclick="zoom(1)">'
+    photodata.innerHTML = '<img src="imagens/galeria-01-b.jpg" width="100px" onmouseover="zoomin(1)" onmouseout="zoomout(1)" onclick="zoom(1)">'
     }
     if (c == 2) {
             let photodata = document.getElementById('foto02')
-            photodata.innerHTML = '<img src="_imagens/galeria-02-b.jpg"  width="100px" onmouseover="zoomin(2)" onmouseout="zoomout(2)" onclick="zoom(2)">'
+            photodata.innerHTML = '<img src="imagens/galeria-02-b.jpg"  width="100px" onmouseover="zoomin(2)" onmouseout="zoomout(2)" onclick="zoom(2)">'
     }
     if (c == 3) {
                 let photodata = document.getElementById('foto03')
-                photodata.innerHTML = '<img src="_imagens/galeria-03-b.jpg" width="100px" onmouseover="zoomin(3)" onmouseout="zoomout(3)" onclick="zoom(3)">'
+                photodata.innerHTML = '<img src="imagens/galeria-03-b.jpg" width="100px" onmouseover="zoomin(3)" onmouseout="zoomout(3)" onclick="zoom(3)">'
     }
     if (c == 4) {
                     let photodata = document.getElementById('foto04')
-                    photodata.innerHTML = '<img src="_imagens/galeria-04-b.jpg" width="100px" onmouseover="zoomin(4)" onmouseout="zoomout(4)" onclick="zoom(4)">'
+                    photodata.innerHTML = '<img src="imagens/galeria-04-b.jpg" width="100px" onmouseover="zoomin(4)" onmouseout="zoomout(4)" onclick="zoom(4)">'
     }
     if (c == 5) {
         let photodata = document.getElementById('foto05')
-        photodata.innerHTML = '<img src="_imagens/galeria-05-b.jpg"  width="100px" onmouseover="zoomin(5)" onmouseout="zoomout(5)" onclick="zoom(5)">'
+        photodata.innerHTML = '<img src="imagens/galeria-05-b.jpg"  width="100px" onmouseover="zoomin(5)" onmouseout="zoomout(5)" onclick="zoom(5)">'
     }    
     if (c == 6) {
             let photodata = document.getElementById('foto06')
-            photodata.innerHTML = '<img src="_imagens/galeria-06-b.jpg"  width="100px" onmouseover="zoomin(6)" onmouseout="zoomout(6)" onclick="zoom(6)">'
+            photodata.innerHTML = '<img src="imagens/galeria-06-b.jpg"  width="100px" onmouseover="zoomin(6)" onmouseout="zoomout(6)" onclick="zoom(6)">'
     }
 }
 
 function zoom (d) {
     if (d == 1) {
     let photobig = document.getElementById('foto-zoom')
-    photobig.innerHTML = '<img src="_imagens/galeria-01.jpg" >'
+    photobig.innerHTML = '<img src="imagens/galeria-01.jpg" >'
     }
     if (d == 2) {
         let photobig = document.getElementById('foto-zoom')
-        photobig.innerHTML = '<img src="_imagens/galeria-02.jpg" >'
+        photobig.innerHTML = '<img src="imagens/galeria-02.jpg" >'
     }
     if (d == 3) {
         let photobig = document.getElementById('foto-zoom')
-        photobig.innerHTML = '<img src="_imagens/galeria-03.jpg" >'
+        photobig.innerHTML = '<img src="imagens/galeria-03.jpg" >'
     }
     if (d == 4) {
         let photobig = document.getElementById('foto-zoom')
-        photobig.innerHTML = '<img src="_imagens/galeria-04.jpg" >'
+        photobig.innerHTML = '<img src="imagens/galeria-04.jpg" >'
     }
     if (d == 5) {
         let photobig = document.getElementById('foto-zoom')
-        photobig.innerHTML = '<img src="_imagens/galeria-05.jpg" >'
+        photobig.innerHTML = '<img src="imagens/galeria-05.jpg" >'
     }
     if (d == 6) {
         let photobig = document.getElementById('foto-zoom')
-        photobig.innerHTML = '<img src="_imagens/galeria-06.jpg" >'
+        photobig.innerHTML = '<img src="imagens/galeria-06.jpg" >'
     }
 }
 
